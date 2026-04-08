b55ef52dd81b44a410ee506c16fb026f6c528d6c
Sabbir <sabbirsamolla51@gmail.com>
2026-04-09 04:47:46 +0600

    affirmation page audio voice add

--- lib/modules/affirmation/view/affirmationview.dart
+++ lib/modules/affirmation/view/affirmationview.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: sort_child_properties_last, deprecated_member_use
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -7,228 +5,306 @@ import 'package:zenzi/core/theme/app_colors.dart';
 import 'package:zenzi/core/theme/app_text_style.dart';
 import 'package:zenzi/core/values/app_assets.dart';
 import 'package:zenzi/core/widgets/themed_scaffold.dart';
+import 'package:zenzi/modules/affirmation/controller/affirmation_controller.dart';
+import 'package:zenzi/modules/affirmation/model/affirmation_model.dart';
 
 class AffirmationView extends StatelessWidget {
   const AffirmationView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(AffirmationController());
+
+    final demoItem = AffirmationModel(
+      id: -1,
+      text: "Loading peaceful thoughts...",
+      authorOrSource: "Connecting to server",
+      isFavorited: false,
+    );
+
     return ThemedScaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 16.h),
-              Row(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: AppColors.componentnormal,
-                    ),
-                    onPressed: () {
-                      Get.back();
-                    },
-                  ),
-                  SizedBox(width: 80.w),
-                  Text('Affirmation', style: AppTextStyle.h9),
-                ],
-              ),
-              SizedBox(height: 16.h),
-              Text(
-                '1 of 90',
-                style: AppTextStyle.button.copyWith(
-                  color: AppColors.secondarycolor,
-                ),
-              ),
-              SizedBox(height: 24.h),
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                    left: 10,
-                    top: 0,
-                    bottom: 0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: Icon(Icons.chevron_left),
-                    ),
-                  ),
+          child: Obx(() {
+            final bool isSkeleton =
+                controller.isLoading.value ||
+                controller.affirmationList.isEmpty;
 
-                  /// RIGHT ARROW
-                  Positioned(
-                    right: 10,
-                    top: 0,
-                    bottom: 0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: Icon(Icons.chevron_right),
-                    ),
-                  ),
-                  Image.asset(
-                    AppAssets.affirmationcard,
-                    width: 350.w,
-                    height: 400.h,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16.w),
-                    child: Text(
-                      'I am rooted in the strength of \nmy ancestors. Their \nresilience flows through me.',
-                      style: AppTextStyle.h7.copyWith(
-                        color: AppColors.primarytext,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Positioned(
-                    left: 10,
-                    top: 0,
-                    bottom: 0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: Icon(Icons.chevron_left),
-                    ),
-                  ),
+            final currentItem = isSkeleton
+                ? demoItem
+                : controller.affirmationList[controller.currentIndex.value];
 
-                  /// RIGHT ARROW
-                  Positioned(
-                    right: 10,
-                    top: 0,
-                    bottom: 0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: Icon(Icons.chevron_right),
-                    ),
-                  ),
+            return Column(
+              children: [
+                SizedBox(height: 16.h),
 
-                  SizedBox(height: 20),
-                  Positioned(
-                    bottom: 90,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 12.w,
-                        vertical: 6.h,
-                      ),
-                      child: Text(
-                        "Strength",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25.r),
-                        color: Color(0xFFFFFFFF).withOpacity(0.20),
-                      ),
+                if (controller.hasError.value)
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10.h),
+                    padding: EdgeInsets.all(8.h),
+                    decoration: BoxDecoration(
+                      color: Colors.red.withValues(alpha: 0.1),
+                      borderRadius: BorderRadius.circular(8.r),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 24.h),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 14.h),
-                      decoration: BoxDecoration(
-                        color: AppColors.primarycolor,
-                        border: Border.all(
-                          color: AppColors.secondarycolor,
-                          width: 1.w,
-                        ),
-                        borderRadius: BorderRadius.circular(30.r),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.favorite_border,
-                            color: Colors.black,
-                            size: 20.sp,
-                          ),
-                          SizedBox(width: 8.w),
-                          Text(
-                            'Save',
-                            style: AppTextStyle.button.copyWith(
-                              color: Colors.black,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.wifi_off, color: Colors.red, size: 16.sp),
+                        SizedBox(width: 8.w),
+                        Expanded(
+                          child: Text(
+                            "No internet connection. Showing offline mode.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 12.sp,
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 16.w),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 14.h),
-                      decoration: BoxDecoration(
-                        color: AppColors.primarycolor,
-                        border: Border.all(
-                          color: AppColors.secondarycolor,
-                          width: 1.w,
                         ),
-                        borderRadius: BorderRadius.circular(30.r),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.share_outlined,
-                            color: Colors.black,
-                            size: 20.sp,
-                          ),
-                          SizedBox(width: 8.w),
-                          Text(
-                            'Share',
-                            style: AppTextStyle.button.copyWith(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 24.h),
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 20.w),
-                decoration: BoxDecoration(
-                  color: AppColors.primarycolor,
-                  borderRadius: BorderRadius.circular(30.r),
-                ),
-                child: Column(
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      '"A person is a person because of other \npeople."',
-                      style: AppTextStyle.h6.copyWith(
-                        color: Color(0xFF2F2F2F),
-                        fontStyle: FontStyle.italic,
+                    IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: AppColors.whitelite,
                       ),
-                      textAlign: TextAlign.center,
+                      onPressed: () => Get.back(),
                     ),
-                    SizedBox(height: 12.h),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 16.w,
-                        vertical: 6.h,
+                    Text('Affirmation', style: AppTextStyle.h9),
+                  ],
+                ),
+
+                SizedBox(height: 16.h),
+
+                Text(
+                  isSkeleton
+                      ? '0 of 0'
+                      : '${controller.currentIndex.value + 1} of ${controller.totalCount.value}',
+                  style: AppTextStyle.button.copyWith(
+                    color: AppColors.secondarycolor,
+                  ),
+                ),
+
+                SizedBox(height: 24.h),
+
+                Opacity(
+                  opacity: isSkeleton ? 0.6 : 1.0,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.asset(
+                        AppAssets.affirmationcard,
+                        width: 350.w,
+                        height: 400.h,
+                        fit: BoxFit.contain,
                       ),
-                      decoration: BoxDecoration(),
-                      child: Text(
-                        '- Ubuntu Philosophy',
-                        style: AppTextStyle.h6.copyWith(
-                          color: Color(0xFF2F2F2F),
-                          fontSize: 16.sp,
+
+                      Positioned(
+                        top: 22.h,
+                        right: 14.w,
+                        child: Obx(
+                          () => IconButton(
+                            onPressed: isSkeleton
+                                ? null
+                                : () {
+                                    if (controller.isSpeaking.value) {
+                                      controller.stopSpeaking();
+                                    } else {
+                                      controller.speakText(
+                                        currentItem.text ?? "",
+                                      );
+                                    }
+                                  },
+                            icon: Icon(
+                              controller.isSpeaking.value
+                                  ? Icons.volume_off_outlined
+                                  : Icons.volume_up_outlined,
+                              color: Colors.white,
+                              size: 28.sp,
+                            ),
+                          ),
                         ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 52.w),
+                        child: Container(
+                          height: 200.h,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10.w,
+                            vertical: 20.h,
+                          ),
+                          child: SingleChildScrollView(
+                            child: Text(
+                              currentItem.text ?? "No Text Available",
+                              style: AppTextStyle.h7.copyWith(
+                                color: AppColors.primarytext,
+                                fontSize: 17.sp,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      Positioned(
+                        left: 8.w,
+                        child: IconButton(
+                          icon: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: Icon(
+                              Icons.chevron_left,
+                              color:
+                                  isSkeleton ||
+                                      controller.currentIndex.value == 0
+                                  ? Colors.grey
+                                  : Colors.black,
+                            ),
+                          ),
+                          onPressed: isSkeleton
+                              ? null
+                              : controller.previousAffirmation,
+                        ),
+                      ),
+
+                      Positioned(
+                        right: 8.w,
+                        child: IconButton(
+                          icon: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: controller.isLoadMore.value
+                                ? SizedBox(
+                                    width: 18.w,
+                                    height: 18.h,
+                                    child: const CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                    ),
+                                  )
+                                : Icon(
+                                    Icons.chevron_right,
+                                    color: isSkeleton
+                                        ? Colors.grey
+                                        : Colors.black,
+                                  ),
+                          ),
+                          onPressed: isSkeleton
+                              ? null
+                              : () => controller.nextAffirmation(),
+                        ),
+                      ),
+
+                      Positioned(
+                        bottom: 90.h,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 12.w,
+                            vertical: 6.h,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25.r),
+                            color: Colors.white.withValues(alpha: 0.2),
+                          ),
+                          child: Text(
+                            currentItem.categoryName,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                if (controller.hasError.value && !controller.isLoading.value)
+                  TextButton.icon(
+                    onPressed: controller.refreshAffirmations,
+                    icon: const Icon(Icons.refresh),
+                    label: const Text("Retry Connection"),
+                  ),
+
+                SizedBox(height: 24.h),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: _buildBtn(
+                        onTap: isSkeleton
+                            ? () {}
+                            : () => controller.toggleFavorite(
+                                currentItem.id,
+                                controller.currentIndex.value,
+                              ),
+                        icon: currentItem.isFavorited == true
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        label: 'Save',
+                        iconColor: currentItem.isFavorited == true
+                            ? Colors.red
+                            : Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 16.w),
+                    Expanded(
+                      child: _buildBtn(
+                        onTap: () {},
+                        icon: Icons.share_outlined,
+                        label: 'Share',
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
-          ),
+
+                SizedBox(height: 24.h),
+
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16.h),
+                  decoration: BoxDecoration(
+                    color: AppColors.primarycolor,
+                    borderRadius: BorderRadius.circular(30.r),
+                  ),
+                  child: Text(
+                    '- ${currentItem.authorOrSource ?? "Unknown"}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBtn({
+    required VoidCallback onTap,
+    required IconData icon,
+    required String label,
+    Color iconColor = Colors.black,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 14.h),
+        decoration: BoxDecoration(
+          color: AppColors.primarycolor,
+          border: Border.all(color: AppColors.secondarycolor),
+          borderRadius: BorderRadius.circular(30.r),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: iconColor, size: 20.sp),
+            SizedBox(width: 8.w),
+            Text(label),
+          ],
         ),
       ),
     );
