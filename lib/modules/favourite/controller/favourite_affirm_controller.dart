24fee9e74637912f3d5f2487c2f2849920db4d40
Sabbir <sabbirsamolla51@gmail.com>
2026-04-09 06:18:24 +0600

    favourites affirm api add

--- lib/modules/favourite/controller/favourite_affirm_controller.dart
+++ lib/modules/favourite/controller/favourite_affirm_controller.dart
@@ -1,44 +1,5 @@
-// import 'package:get/get.dart';
-// import 'package:zenzi/core/network/services/api_services.dart';
-// import 'package:zenzi/modules/favourite/model/favourite_affirmation_model.dart';
+import 'dart:developer';
 
-// class AffirmationController extends GetxController {
-//   final ApiServices apiServices = ApiServices();
-
-//   var isLoading = true.obs;
-//   var affirmationList = <AffirmationModel>[].obs;
-
-//   @override
-//   void onInit() {
-//     getAffirmations();
-//     super.onInit();
-//   }
-
-//   Future<void> getAffirmations() async {
-//     try {
-//       isLoading(true);
-
-//       final response = await apiServices.get(
-//         '/api/v1/content/favorites/affirmations/',
-//       );
-
-//       if (response.statusCode == 200) {
-//         final List data = response.data['results'];
-//         affirmationList.assignAll(
-//           data.map((e) => AffirmationModel.fromJson(e)).toList(),
-//         );
-//       }
-//     } catch (e) {
-//       Get.snackbar(
-//         "Error",
-//         e.toString(),
-//         snackPosition: SnackPosition.BOTTOM,
-//       );
-//     } finally {
-//       isLoading(false);
-//     }
-//   }
-// }
 import 'package:get/get.dart';
 import 'package:zenzi/core/network/services/api_services.dart';
 import 'package:zenzi/modules/favourite/model/favourite_affirmation_model.dart';
@@ -55,33 +16,28 @@ class FavouriteAffirmController extends GetxController {
     super.onInit();
   }
 
-Future<void> getAffirmations() async {
-  try {
-    isLoading(true);
+  Future<void> getAffirmations() async {
+    try {
+      isLoading.value = true;
 
-    final response = await apiServices.get(
-      '/api/v1/content/favorites/affirmations/',
-       requireAuth: true
-    );
+      final response = await apiServices.get(
+        '/api/v1/content/favorites/affirmations/',
+        requireAuth: true,
+      );
+      final body = response.data;
+      log("Affirmation API Response: $body");
+      final List dataList = body['data']['results'];
 
-    // লক্ষ্য করুন: response.data-এর ভেতরে আবার একটি 'data' ম্যাপ আছে
-    if (response.statusCode == 200 && response.data != null) {
-      // আপনার API স্ট্রাকচার অনুযায়ী: response.data['data']['results']
-      final List? dataList = response.data['data']['results'];
-      
-      if (dataList != null) {
-        affirmationList.assignAll(
-          dataList.map((e) => AffirmationModel.fromJson(e)).toList(),
-        );
-        print("Data loaded successfully: ${affirmationList.length}");
-      } else {
-        affirmationList.clear();
-      }
+      affirmationList.clear();
+
+      affirmationList.addAll(
+        dataList.map((e) => AffirmationModel.fromJson(e)).toList(),
+      );
+      log("Data loaded successfully: ${affirmationList.length}");
+    } catch (e) {
+      log("Affirmation API Error: $e");
+    } finally {
+      isLoading.value = false;
     }
-  } catch (e) {
-    print("Affirmation API Error: $e");
-  } finally {
-    isLoading(false);
   }
-}
 }
--- lib/modules/favourite/view/affirm.dart
+++ lib/modules/favourite/view/affirm.dart
@@ -1,72 +1,9 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:zenzi/core/theme/app_colors.dart';
-// import 'package:zenzi/modules/favourite/widgets/affiirm_card.dart';
-// import 'package:zenzi/modules/favourite/widgets/custom_share_bottom_sheet.dart';
-
-// class Affirm extends StatelessWidget {
-//   const Affirm({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         SizedBox(height: 20.h),
-//         Column(
-//           children: [
-//             AffirmCard(
-//               text:
-//                   "I am rooted in the strength of my ancestors. Their resilience flows through me.",
-//               backgroundColor: AppColors.componentnormal,
-//               onLongPress: () => CustomShareBottomSheet.show(
-//                 context,
-//                 "I am rooted in the strength of my ancestors. Their resilience flows through me.",
-//               ),
-//             ),
-//             SizedBox(height: 16.h),
-//             AffirmCard(
-//               text:
-//                   "With every breath, I embrace my heritage and honor the journey of those who came before me.",
-//               backgroundColor: AppColors.coreprimarydark,
-//               onLongPress: () => CustomShareBottomSheet.show(
-//                 context,
-//                 "With every breath, I embrace my heritage and honor the journey of those who came before me.",
-//               ),
-//             ),
-//             SizedBox(height: 16.h),
-//             AffirmCard(
-//               text:
-//                   "I am rooted in the strength of my ancestors. Their resilience flows through me.",
-//               backgroundColor: AppColors.componentnormal,
-//               onLongPress: () => CustomShareBottomSheet.show(
-//                 context,
-//                 "I am rooted in the strength of my ancestors. Their resilience flows through me.",
-//               ),
-//             ),
-//             SizedBox(height: 16.h),
-//             AffirmCard(
-//               text:
-//                   "I am rooted in the strength of my ancestors. Their resilience flows through me.",
-//               backgroundColor: AppColors.coreprimarydark,
-//               onLongPress: () => CustomShareBottomSheet.show(
-//                 context,
-//                 "I am rooted in the strength of my ancestors. Their resilience flows through me.",
-//               ),
-//             ),
-//             // MusicCardWidget(isFav: true),
-//             SizedBox(height: 30.h),
-//           ],
-//         ),
-//       ],
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:zenzi/core/theme/app_colors.dart';
 import 'package:zenzi/modules/favourite/controller/favourite_affirm_controller.dart';
-import 'package:zenzi/modules/favourite/model/favourite_affirmation_model.dart'; 
+import 'package:zenzi/modules/favourite/model/favourite_affirmation_model.dart';
 import 'package:zenzi/modules/favourite/widgets/affiirm_card.dart';
 import 'package:zenzi/modules/favourite/widgets/custom_share_bottom_sheet.dart';
 
@@ -104,9 +41,9 @@ class Affirm extends StatelessWidget {
               final AffirmationModel item = controller.affirmationList[index];
 
               return AffirmCard(
-                text: item.text!,
-                backgroundColor: index % 2 == 0 
-                    ? AppColors.componentnormal 
+                text: item.text,
+                backgroundColor: index % 2 == 0
+                    ? AppColors.componentnormal
                     : AppColors.coreprimarydark,
                 onLongPress: () => CustomShareBottomSheet.show(
                   context,
@@ -115,7 +52,6 @@ class Affirm extends StatelessWidget {
               );
             },
           ),
-          SizedBox(height: 30.h),
         ],
       );
     });
